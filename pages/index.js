aa09befda00bd6d712585f519007d92781c1e736
Paulo Magaldi <paulomagaldi@gmail.com>
2021-01-26 00:18:57 -0300

    Aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,50 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width:100%;
+  max-width:350px;
+  padding-top:45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>Outros Quizes</Widget.Header>
+          <Widget.Content>            
+            <a href='https://aluraquiz-css.omariosouto.vercel.app/'>
+              <p>Quiz CSS - Mario Souto</p>
+            </a>
+            <a href='https://css-quiz.higorpo.vercel.app/'>
+              <p>Quiz CSS - Higor Oliveira</p>
+            </a>
+          </Widget.Content>          
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl='https://github.com/pmagaldi' />
+    </QuizBackground>
+  )
 }
